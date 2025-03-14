ed1641c19cdb818b92cc4135a698a8419c01e145
Acus <acus1304pgphcm@gmail.com>
2025-03-14 23:05:14 +0700

    Phuc | Update code

--- prisma/migrations/20250309084218_db_3_add_activated/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `is_verified` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "is_verified",
-ADD COLUMN     "is_activated" BOOLEAN NOT NULL DEFAULT false;
