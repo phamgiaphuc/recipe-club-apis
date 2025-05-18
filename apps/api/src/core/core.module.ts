320a267c7f2fd504f0c2f428c14ad1ef77646241
Acus <acuscodinghcm@gmail.com>
2025-05-19 00:14:28 +0700

    Phuc | Update code

--- apps/api/src/core/core.module.ts
+++ apps/api/src/core/core.module.ts
@@ -1,8 +1,7 @@
 import { IngredientModule } from "@app/api/core/ingredient/ingredient.module";
 import { UserModule } from "@app/api/core/user/user.module";
 import { Module } from "@nestjs/common";
-import { RecipeModule } from './recipe/recipe.module';
-
+import { RecipeModule } from "./recipe/recipe.module";
 
 @Module({
   imports: [IngredientModule, UserModule, RecipeModule],
