8ecda24d84fe93e182d887f609002c5ff44311f5
Quỳnh Anh <quynhanhhoang572004@gmail.com>
2025-05-25 02:09:48 +0700

    Quynh Anh | Update code

--- apps/api/src/core/recipe/recipe.controller.ts
+++ apps/api/src/core/recipe/recipe.controller.ts
@@ -1,31 +1,47 @@
-import { Controller, Get, Request, UseGuards, Param } from "@nestjs/common";
+import { Controller, Get, Request, UseGuards, Param, Query } from "@nestjs/common";
 import { RecipeService } from "./recipe.service";
 import {
   ApiTags,
   ApiOkResponse,
   ApiUnauthorizedResponse,
   ApiBearerAuth,
+  ApiQuery
 } from "@nestjs/swagger";
 import { AtGuard } from "@app/api/base/auth/guard/at.guard";
 
 @ApiTags("Recipes")
-@ApiBearerAuth()
-@UseGuards(AtGuard)
 @Controller("recipes")
 export class RecipeController {
   constructor(private readonly recipeService: RecipeService) {}
 
   @Get("recommend")
+  @ApiBearerAuth()
+@UseGuards(AtGuard)
   @ApiOkResponse({ description: "get recipe based on their ingredient" })
   @ApiUnauthorizedResponse({ description: "Unauthorized" })
   async recommend(@Request() req) {
     return this.recipeService.recommendRecipes(req.user.user_id);
   }
-
+   @Get("search")
+  @ApiOkResponse({ description: "Search for recipes by title or ingredient name" })
+  @ApiQuery({ name: "q", type: String, required: true, description: "Search keyword" })
+  async search(@Query("q") query: string) {
+    return this.recipeService.searchRecipes(query);
+  }
   @Get(":id")
+  @ApiBearerAuth()
+@UseGuards(AtGuard)
   @ApiOkResponse({ description: "Get full recipe detail by ID" })
 @ApiUnauthorizedResponse({ description: "Unauthorized" })
   async getById(@Request() req,@Param("id") id: string) {
      return this.recipeService.getRecipeById(id, req.user.user_id);
   }
+
+
+@Get("recommend/guest")
+@ApiOkResponse({ description: "Get public recipe recommendations (no login needed)" })
+async recommendForGuest() {
+  return this.recipeService.recommendRandomRecipes();
+}
+
 }
